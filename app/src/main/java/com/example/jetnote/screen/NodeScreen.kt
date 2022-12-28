846e9fb15edf0b5ecd96d44c541ab9ec1b2db10f
vuthisak <khyvuthisak@gmail.com>
2022-12-29 00:47:45 +0700

    first commit

---
+++ app/src/main/java/com/example/jetnote/screen/NodeScreen.kt
@@ -0,0 +1,45 @@
+package com.example.jetnote.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.R
+
+@Composable
+fun NoteScreen() {
+    Column(modifier = Modifier.padding()) {
+        TopAppBar(title = {
+            Text(text = stringResource(id = R.string.app_name))
+        }, actions = {
+            Icon(
+                imageVector = Icons.Rounded.Notifications,
+                contentDescription = "Notifications"
+            )
+        }, backgroundColor = Color(color = 0xFFDADFE3))
+
+        // Content
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview() {
+    NoteScreen()
+}
