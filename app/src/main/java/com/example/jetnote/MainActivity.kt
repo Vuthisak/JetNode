bc21bdc2e9dc7649146af428180ab0284dd2395e
vuthisak <khyvuthisak@gmail.com>
2023-03-07 01:06:08 +0700

    added viewmodel

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+<<<<<<< HEAD
 import androidx.compose.ui.ExperimentalComposeUiApi
+=======
+import androidx.compose.runtime.Composable
+>>>>>>> added viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+<<<<<<< HEAD
 import com.example.jetnote.data.NotesData
+=======
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.jetnote.data.NotesDataSource
+>>>>>>> added viewmodel
 import com.example.jetnote.screen.NoteScreen
+import com.example.jetnote.screen.NoteViewModel
 import com.example.jetnote.ui.theme.JetNoteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,16 +39,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateOf(NotesData().loadNotes().toMutableStateList())
-                    }
-                    NoteScreen(notes.value, onAddNote = {
-                        notes.value.add(it)
-                    }, onRemoveNote = {
-                        notes.value.remove(it)
-                    })
+                    val viewModel: NoteViewModel by viewModels()
+                    NotesApp(viewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    NoteScreen(noteViewModel.getAllNotes(), onAddNote = {
+        noteViewModel.addNode(it)
+    }, onRemoveNote = {
+        noteViewModel.removeNode(it)
+    })
 }
