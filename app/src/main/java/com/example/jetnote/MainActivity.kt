83cf9100bbbd219a4ea86d88ab8ba96b62c00622
vuthisak <khyvuthisak@gmail.com>
2023-03-29 00:01:39 +0700

    removed conflict

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -7,21 +7,10 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-<<<<<<< HEAD
 import androidx.compose.ui.ExperimentalComposeUiApi
-=======
 import androidx.compose.runtime.Composable
->>>>>>> added viewmodel
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-<<<<<<< HEAD
-import com.example.jetnote.data.NotesData
-=======
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.jetnote.data.NotesDataSource
->>>>>>> added viewmodel
 import com.example.jetnote.screen.NoteScreen
 import com.example.jetnote.screen.NoteViewModel
 import com.example.jetnote.ui.theme.JetNoteTheme
