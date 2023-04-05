f3860948e88065d4e5a4c68208668c05e77e112b
vuthisak <khyvuthisak@gmail.com>
2023-04-06 00:45:07 +0700

    Update NoteDatabaseDao

--- app/src/main/java/com/example/jetnote/model/NoteDatabaseDao.kt
+++ app/src/main/java/com/example/jetnote/model/NoteDatabaseDao.kt
@@ -1,5 +1,24 @@
 package com.example.jetnote.model
 
-interface NoteDatabaseDao {
+import androidx.room.*
 
+@Dao
+interface NoteDatabaseDao {
+    @Query("SELECT * from notes_tbl")
+    fun getNotes(): List<Note>
+
+    @Query("SELECT * from notes_tbl where id =:id")
+    fun getNoteById(id: String): Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(note: Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(note: Note)
+
+    @Query("DELETE from notes_tbl")
+    fun deleteAll()
+
+    @Delete
+    fun deleteNote(note: Note)
 }
