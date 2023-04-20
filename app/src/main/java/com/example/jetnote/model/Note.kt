f45e4cdf263ab643a4c1fa636555d2aa11aa56cb
vuthisak <khyvuthisak@gmail.com>
2023-04-21 00:32:11 +0700

    Added Date and UUID converter

--- app/src/main/java/com/example/jetnote/model/Note.kt
+++ app/src/main/java/com/example/jetnote/model/Note.kt
@@ -3,7 +3,6 @@ package com.example.jetnote.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.TypeConverters
 import java.time.Instant
 import java.util.*
 
